b054a997015495ac86f46bed43d6af0b2fc5c17f
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2022-07-03 17:14:19 -0700

    shraousy - signup pages sprint 1 v1

--- app/src/main/java/com/xd/lifepuzzle/SignupActivity.java
+++ app/src/main/java/com/xd/lifepuzzle/SignupActivity.java
@@ -1,6 +1,8 @@
 package com.xd.lifepuzzle;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,33 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void sendMale(View view)
+    {
+        // do something when "male" is called
+        view.setEnabled(false);
+        Button b = (Button) view;
+        //
+        String s = getString(R.string.male);
+        b.setText(s);
+    }
+    /**called when the user taps the "female" button*/
+    public void sendFemale(View view)
+    {
+        // do something when "male" is called
+        view.setEnabled(false);
+        Button b = (Button) view;
+        String s = getString(R.string.female);
+        b.setText(s);
+    }
+    /**called when the user taps the "Sign Up" button*/
+    public void sign_user_up(View view)
+    {
+        // do something when "male" is called
+        view.setEnabled(false);
+        Button b = (Button) view;
+        String s = getString(R.string.button_signUp);
+        b.setText(s);
     }
 }
