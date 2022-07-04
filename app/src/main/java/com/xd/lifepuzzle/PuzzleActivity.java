cdd7743922276c018c7400838fc9891318ff6676
mta118 <mta118@sfu.ca>
2022-07-04 16:20:18 -0700

    Created the Menu Page and added the "label name" for each page in androidManifest.xml

--- app/src/main/java/com/xd/lifepuzzle/PuzzleActivity.java
+++ app/src/main/java/com/xd/lifepuzzle/PuzzleActivity.java
@@ -1,6 +1,8 @@
 package com.xd.lifepuzzle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,9 @@ public class PuzzleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
 
+        Intent i = getIntent();
+        String puzzleID = i.getStringExtra("puzzleID");
 
+        ((TextView)findViewById(R.id.textView9)).setText(puzzleID);
     }
 }
