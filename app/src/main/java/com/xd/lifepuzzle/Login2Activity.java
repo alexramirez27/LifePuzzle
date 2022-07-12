46f5372414af1b31e6b815ed2ab270e138ca3fce
alexramirez27 <alejandrp98@gmail.com>
2022-07-12 02:35:11 -0700

    Commit test by Alex

--- app/src/main/java/com/xd/lifepuzzle/Login2Activity.java
+++ app/src/main/java/com/xd/lifepuzzle/Login2Activity.java
@@ -9,5 +9,7 @@ public class Login2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_2);
+
+        // Comment made by Alex!!!!!!!
     }
 }
