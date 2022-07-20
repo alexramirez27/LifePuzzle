672fa441c9472d665a860369c07a23712554a5ee
steven <wca78@sfu.ca>
2022-07-20 12:13:20 -0700

    puzzlegame

--- app/src/main/java/com/xd/lifepuzzle/MainActivity.java
+++ app/src/main/java/com/xd/lifepuzzle/MainActivity.java
@@ -8,12 +8,46 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static java.lang.Math.abs;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        setContentView(R.layout.activity_first_installation_page);
+        AssetManager am = getAssets();
+        try {
+            final String[] files  = am.list("img");
+
+            GridView grid = findViewById(R.id.grid);
+            grid.setAdapter(new ImageAdapter(this));
+        } catch (IOException e) {
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT);
+        }
     }
 //
 //    /**
