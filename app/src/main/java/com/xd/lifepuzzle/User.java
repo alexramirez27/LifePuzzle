264e0376836a57c57a44e80e998d3ee0e80eec58
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2022-07-20 23:52:29 -0700

    database by alex

---
+++ app/src/main/java/com/xd/lifepuzzle/User.java
@@ -0,0 +1,20 @@
+package com.xd.lifepuzzle;
+
+public class User {
+
+    public String name, age, email, phoneNo, gender;
+
+    public User(){
+
+    }
+
+    public User(String name, String age, String email, String phoneNo, String gender) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.gender = gender;
+    }
+
+
+}
